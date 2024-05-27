5374cf9e8414de2f86f9c8a1265238175c637585
Mahmoud Saad <62154958+mahmoudmohamedsaad@users.noreply.github.com>
2024-05-27 14:37:10 +0300

    [2485] [Time Beats: 34.60%] [Memory Beats: 79.05%] - LeetPush

---
+++ 2485-Find-the-Pivot-Integer.js
@@ -0,0 +1,9 @@
+function pivotInteger(n) {
+  if (n === 1) return 1
+  for (let i = 2; i <= n; i++) {
+    const sum = (i * (1 + i)) / 2
+    const sumPivot = ((n - i + 1) * (i + n)) / 2
+    if(sum === sumPivot) return i
+  }
+  return -1
+};
