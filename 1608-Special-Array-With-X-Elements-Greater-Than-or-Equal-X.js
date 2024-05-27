ca1dfe66f810478b93e8121924587e203862d84d
Mahmoud Saad <62154958+mahmoudmohamedsaad@users.noreply.github.com>
2024-05-27 14:34:48 +0300

    [1608] [Time Beats: 58.33%] [Memory Beats: 50.93%] - LeetPush

---
+++ 1608-Special-Array-With-X-Elements-Greater-Than-or-Equal-X.js
@@ -0,0 +1,10 @@
+var specialArray = function (nums) {
+    for (i = 0; i <= nums.length; i++) {
+        let c = 0;
+        for (let num of nums) {
+            num >= i && ++c;
+        }
+        if (c === i) return i;
+    }
+    return -1;
+};
