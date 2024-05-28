af637eb90e1b3206c2eb046e8a6df752a636f6bb
Mahmoud Saad <62154958+mahmoudmohamedsaad@users.noreply.github.com>
2024-05-28 12:19:22 +0300

    [1208] [Time Beats: 25.69%] [Memory Beats: 65.14%] - LeetPush

---
+++ 1208-Get-Equal-Substrings-Within-Budget.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @param {number} maxCost
+ * @return {number}
+ */
+var equalSubstring = function ( s, t, maxCost ) {
+    let i = 0, j = 0, cost = 0, maxLen = 0;
+    while ( j < t.length ) {
+        const nextCost = Math.abs(s.charCodeAt(j) - t.charCodeAt(j));
+        if ( cost + nextCost <= maxCost ) {
+            j++;
+            cost += nextCost;
+        } else {
+            maxLen = Math.max(maxLen, j - i);
+            if ( i === j ) j++;
+            else cost -= Math.abs(s.charCodeAt(i) - t.charCodeAt(i));
+            i++;
+        }
+    }
+    maxLen = Math.max(maxLen, j - i);
+    return maxLen;
+};
